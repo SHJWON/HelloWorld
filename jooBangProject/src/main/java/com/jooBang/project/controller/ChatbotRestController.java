3f5f4097fba951c06e13b5ca52498fe78f638c45
sohyemin <shm8201@naver.com>
2023-02-06 21:11:18 +0900

    chatbot +

---
+++ jooBangProject/src/main/java/com/jooBang/project/controller/ChatbotRestController.java
@@ -0,0 +1,20 @@
+package com.jooBang.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jooBang.project.service.ChatbotService;
+
+@RestController
+public class ChatbotRestController {
+	@Autowired
+	private ChatbotService service;
+	
+	@RequestMapping("/chatbot")
+	public String chatbot(@RequestParam("message") String message ) {
+		System.out.println("연락");
+		return service.main(message);
+	}
+}
