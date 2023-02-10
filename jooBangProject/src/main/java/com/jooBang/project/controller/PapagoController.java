2591fd7e31c59d9cd9d5b1278633172badddc2f3
KANG2860 <tjdwls2860@naver.com>
2023-02-10 13:15:42 +0900

    222

---
+++ jooBangProject/src/main/java/com/jooBang/project/controller/PapagoController.java
@@ -0,0 +1,34 @@
+package com.jooBang.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jooBang.project.service.PapagoService;
+
+@Controller
+public class PapagoController {
+	@Autowired
+	private PapagoService PService;
+	
+	@ResponseBody
+	@RequestMapping("/papago/en")
+	public String en(@RequestParam("test") String test,Model model ) {
+		String html=test;
+		String result=PService.translateToEngHtml(html);
+	
+		return result;
+	}
+	@ResponseBody
+	@RequestMapping("/papago/ko")
+	public String ko(@RequestParam("test") String test,Model model ) {
+		String html=test;
+		String result=PService.translateToKoHtml(html);
+	
+		return result;
+	}
+	
+}
