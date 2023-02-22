f61d5906c8849f1457556278f801990ac2d350dc
SHJWON <sjw3344@gmail.com>
2023-02-22 11:31:53 +0900

    경로

--- jooBangProject/src/main/java/com/jooBang/project/WebConfig.java
+++ jooBangProject/src/main/java/com/jooBang/project/WebConfig.java
@@ -9,10 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:///usr/local/project/product_images/"); // 각자 외부경로 적용 필!
+		.addResourceLocations("file:////usr/local/project/product_images/image/"); 
 		//.addResourceLocations("file:////Users/ddukco/springWorkspace/images/"); // 각자 외부경로 적용 필!
+		//registry.addResourceHandler("/audio/**")
+	    //.addResourceLocations("file:///C:/springWorkspace/upload/");
 		registry.addResourceHandler("/audio/**")
-		.addResourceLocations("file:///C:/springWorkspace/upload/");
+		.addResourceLocations("file:////usr/local/project/upload/");
 		
 	}
 }
