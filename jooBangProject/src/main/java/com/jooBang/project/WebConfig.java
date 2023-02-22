0f0d0debf63aee995f218e3bf7c4736a160c31e3
SHJWON <sjw3344@gmail.com>
2023-02-22 12:34:28 +0900

    이미지

--- jooBangProject/src/main/java/com/jooBang/project/WebConfig.java
+++ jooBangProject/src/main/java/com/jooBang/project/WebConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:////usr/local/project/product_images/image/"); 
-		//.addResourceLocations("file:////Users/ddukco/springWorkspace/images/"); // 각자 외부경로 적용 필!
-		//registry.addResourceHandler("/audio/**")
-	    //.addResourceLocations("file:///C:/springWorkspace/upload/");
+		registry.addResourceHandler("/image/**")
+		//.addResourceLocations("file:////usr/local/project/product_images/image/"); 
+		.addResourceLocations("file:////Users/ddukco/springWorkspace/images/"); // 각자 외부경로 적용 필!
 		registry.addResourceHandler("/audio/**")
-		.addResourceLocations("file:////usr/local/project/upload/");
+	    .addResourceLocations("file:///C:/springWorkspace/upload/");
+		//registry.addResourceHandler("/audio/**")
+		//.addResourceLocations("file:////usr/local/project/upload/");
 		
 	}
 }
